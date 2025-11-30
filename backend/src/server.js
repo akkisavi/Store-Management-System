51290c0a92f507eef48d686ecc240150ba6bf373
akkisavi <akshaygosavi621@gmail.com>
2025-11-30 23:17:25 +0530

    removed comment

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"], // your frontend URL
+    origin: ["http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
